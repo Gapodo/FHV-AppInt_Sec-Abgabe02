256d8a84b52f41cc8611278c03cf16cf280c3f0d
Michael Amann <gapodo@datenclown.at>
2022-03-12 17:43:58 +0100

    Corrected comment in ShopDTO

--- DTO/ShopDTO.cs
+++ DTO/ShopDTO.cs
@@ -2,11 +2,9 @@ namespace a02_shopsystem.DTO
 {
     public partial class ShopDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; } = null!;
 
-        // Shop is excluded, Shop is handled via the ShopId (foreign key relating to the shop)
-        // ShopId will be provided as a url parameter
+        // Articles are excluded (queryable seperately)
     }
 }
-// TODO might not be needed!
