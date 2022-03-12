6c28374da25ece2a3362f60380fa2d9f323543be
Michael Amann <gapodo@datenclown.at>
2022-03-12 01:54:58 +0100

    added Swagger

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using a02_shopsystem.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("ShopsystemDatabase");
 
@@ -12,5 +13,14 @@ builder.Services.AddDbContext<ShopsystemContext>(
 );
 
 var app = builder.Build();
+
+// only deploy swagger in dev env
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapControllers();
+
 app.Run();
