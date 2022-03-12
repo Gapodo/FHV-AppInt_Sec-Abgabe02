fd9ae7baaa58c8a9d85a85521a754157618b5c95
Michael Amann <gapodo@datenclown.at>
2022-03-12 01:55:25 +0100

    removed unneccesary default from ArticleDTO

--- DTO/ArticleDTO.cs
+++ DTO/ArticleDTO.cs
@@ -7,7 +7,7 @@ namespace a02_shopsystem.DTO
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public decimal EuroPrice { get; set; } = 0;
+        public decimal EuroPrice { get; set; }
 
         // Shop is excluded, Shop is handled via the ShopId (foreign key relating to the shop)
         // ShopId will be provided as a url parameter
